3fdde431d31ea2af7dc26007a66e1a9ff845e378
Bill <bill.chandos@gmail.com>
2020-11-19 00:57:30 +0000

    add authenticateAdmin hook

--- src/routes/jwt.js
+++ src/routes/jwt.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
+const sequelize = require('../models');
+const User = sequelize.models.User;
 
 const authenticateToken = async (req, res, next) => {
     // Gather the jwt access token from the request header
@@ -26,6 +28,33 @@ const generateAccessToken = (userId) => {
     return jwt.sign({userId}, process.env.TOKEN_SECRET, { expiresIn: '12h' });
   }
 
+  
+const authenticateAdmin = async (req, res, next) => {
+    // Gather the jwt access token from the request header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) {
+        res.status(401);
+        return res.json({
+            status: 'Unauthorized',
+            message: 'JSON Web Token not found'
+        })
+    }
+    try {
+        const payload = await jwtVerify(token, process.env.TOKEN_SECRET);
+        req.jwtPayload = payload;
+        const user = await User.findByPk(payload.userId);
+        if (!user.admin) {
+            throw new Error();
+        } else {
+            console.log(`User ID ${payload.userId} (admin) authenticated with JWT, expires ${new Date(payload.exp*1000)}`);
+            next(); // pass the execution off to whatever request the client intended
+        }
+    } catch (err) {
+        return res.sendStatus(403);
+    }
+}
+    
 const jwtVerify = (token, secret) => {
     // wrap jsonwebtoken verify() method in a Promise interface
     // so it can be used with async / await
@@ -39,4 +68,4 @@ const jwtVerify = (token, secret) => {
     });
 }
 
-module.exports = {authenticateToken, generateAccessToken};
+module.exports = {authenticateToken, generateAccessToken, authenticateAdmin};
